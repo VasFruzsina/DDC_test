eceba0dc1fc4f708c33e271a4b983236a32cfa1e
VasFruzsina <vs.frzsn@gmail.com>
2025-08-30 17:14:36 +0200

    l1/feature_4: Form ID generation, submit event

--- DDC_test/src/components/composables/useProjectStorage.ts
+++ DDC_test/src/components/composables/useProjectStorage.ts
@@ -1,8 +1,8 @@
-function setItem<T>(key: string, value: T): void {
+export function setItem<T>(key: string, value: T): void {
   localStorage.setItem(key, JSON.stringify(value));
 }
 
-function getItem<T>(key: string): T | null {
+export function getItem<T>(key: string): T | null {
   const item = localStorage.getItem(key);
-  return item ? JSON.parse(item) as T : null;
+  return item ? (JSON.parse(item) as T) : null;
 }
--- DDC_test/src/components/level1/Form.vue
+++ DDC_test/src/components/level1/Form.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
+import { getItem, setItem } from "../composables/useProjectStorage";
 import AppButton from "../UI/AppButton.vue";
 import AppInput from "../UI/AppInput.vue";
 import AppTextarea from "../UI/AppTextarea.vue";
-
 
 import { ref } from "vue";
 
@@ -11,10 +11,40 @@ const description = ref<string | undefined>(undefined);
 const startDate = ref<string | undefined>(undefined);
 const budget = ref<number | null>(null);
 
+interface ProjectForm {
+  id: number;
+  name: string;
+  description: string;
+  startDate: string;
+  budget: number | null;
+}
+
+function nextID(key = 'projectForm:lastId'): number {
+  const raw = localStorage.getItem(key)
+  const last = raw ? parseInt(raw, 10) : 0
+  const next = last + 1
+  localStorage.setItem(key, String(next))
+  return next
+}
+
+function onSubmit() {
+
+  const data: ProjectForm = {
+    id: nextID(),
+    name: name.value ?? '',
+    description: description.value ?? '',
+    startDate: startDate.value ?? '',
+    budget: budget.value ?? null,
+  }
+
+  const list = getItem<ProjectForm[]>('projectForms') ?? []
+  list.push(data)
+  setItem('projectForms', list)
+}
 </script>
 
 <template>
-  <form class="flex flex-col gap-4">
+  <form class="flex flex-col gap-4" @submit="onSubmit">
     <div>
       <label for="name" class="block text-sm font-medium">Projekt neve</label>
       <AppInput id="name" v-model="name" type="text" />
@@ -34,6 +64,6 @@ const budget = ref<number | null>(null);
       <label for="budget" class="block text-sm font-medium">Költségvetés</label>
       <AppInput id="budget" v-model.number="budget" type="number" />
     </div>
-    <AppButton type="submit" subtitle="Submit"/>
+    <AppButton type="submit" subtitle="Mentés" />
   </form>
 </template>
