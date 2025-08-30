18a8591813d15afc2726c118deac8e6320a6b980
VasFruzsina <vs.frzsn@gmail.com>
2025-08-30 15:15:28 +0200

    l1/feature_4: pinia, localstorage base

--- DDC_test/src/components/UI/AppInput.vue
+++ DDC_test/src/components/UI/AppInput.vue
@@ -2,7 +2,6 @@
 const model = defineModel<string | number | null | undefined>()
 
 const props = defineProps<{
-  id?: string
   type?: 'text' | 'date' | 'number'
   placeholder?: string
 }>()
@@ -10,7 +9,6 @@ const props = defineProps<{
 
 <template>
   <input
-    :id="id"
     :type="type"
     v-model="model"
     :placeholder="placeholder"
