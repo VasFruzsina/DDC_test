42f7399a5596cec5a85f8b56885fb3a738141b93
VasFruzsina <vs.frzsn@gmail.com>
2025-08-30 12:39:45 +0200

    l1/feature_2: appInput, appTextarea, Form component

---
+++ DDC_test/src/components/UI/AppInput.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const model = defineModel<string | number | null | undefined>()
+
+const props = defineProps<{
+  id?: string
+  type?: 'text' | 'date' | 'number'
+  placeholder?: string
+}>()
+</script>
+
+<template>
+  <input
+    :id="id"
+    :type="type"
+    v-model="model"
+    :placeholder="placeholder"
+    class="w-full rounded-md border"
+  />
+</template>
--- DDC_test/src/components/UI/AppTextarea.vue
+++ DDC_test/src/components/UI/AppTextarea.vue
@@ -1,3 +1,3 @@
 <template>
-  <textarea></textarea>
+  <textarea class="rounded border"></textarea>
 </template>
